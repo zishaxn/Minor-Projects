be076bbfae32996b584180454fa3e37c38643fab
zishan chaudhary <zishan8291@gmail.com>
2023-10-23 16:58:54 +0530

    add-contact &  delete-contact functionality added

--- Contact-list-App/index.js
+++ Contact-list-App/index.js
@@ -1,3 +1,5 @@
+// setting up server requirements
+const { log } = require("console");
 const express = require("express");
 const path = require("path");
 const port = 7600;
@@ -5,6 +7,9 @@ const app = express();
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+
+// middleware so that we get name and phone number in a key-value pair format
+app.use(express.urlencoded());
 
 // setting up directory for static files for styling & functioning
 // Serve static files from the 'assets' directory
@@ -19,6 +24,40 @@ app.use(
     },
   })
 );
+
+app.post("/", (req, res) => {
+  contacts.push(req.body);
+  console.log("Added", req.body);
+  //here we will get the form data back from the request(as user is making request from their side)
+  // and then that request will have body in which there will be data in the decoded form.
+  // And we are pushing it into the contact list array.
+  return res.redirect("back");
+});
+
+// for deleting the entries in contact list
+app.get("/delete-contact", (req, res) => {
+  let returnedPhone = req.query.phone;
+  let matchedIndex = contacts.findIndex(
+    (contact) => contact.phone == returnedPhone
+  );
+  if (matchedIndex != -1) {
+    console.log(`Deleted: ,
+     ${contacts[matchedIndex].name} : ${contacts[matchedIndex].phone}`);
+    contacts.splice(matchedIndex, 1);
+  }
+  return res.redirect("back");
+});
+
+// app.get("/delete-contact", (req, res) => {
+//   let phoneReturned = req.query.phone;
+//   let contactIndex = contacts.findIndex(
+//     (contact) => contact.phone == phoneReturned
+//   );
+//   if (contactIndex != -1) {
+//     contacts.splice(contactIndex, 1);
+//   }
+//   return res.redirect("back");
+// });
 
 var contacts = [
   {
