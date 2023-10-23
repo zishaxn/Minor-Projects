08047d376ac64776f5513e1c8917c5accc97d546
zishan chaudhary <zishan8291@gmail.com>
2023-10-23 15:56:40 +0530

    rendered a contact list stored at the server using express

--- Contact-list-App/index.js
+++ Contact-list-App/index.js
@@ -3,6 +3,34 @@ const path = require("path");
 const port = 7600;
 const app = express();
 
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+var contacts = [
+  {
+    name: "zishan",
+    phone: "8291533333",
+  },
+  {
+    name: "han",
+    phone: "8666533333",
+  },
+  {
+    name: "ishan",
+    phone: "1755533333",
+  },
+  {
+    name: "zian",
+    phone: "22222333",
+  },
+];
+
+app.get("/", (req, res) => {
+  res.render('home', {
+    contact_list: contacts,
+  })
+});
+
 app.listen(port, (err) => {
   if (err) {
     console.log(err);
